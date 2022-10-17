6754337b4b60c1bc0eae7157d1a6862cbf6fa5f8
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-10-17 18:31:10 +0300

    add Sem000 - Задача 0

---
+++ Sem_000/Program.cs
@@ -0,0 +1,8 @@
+﻿// 00 Напишите программу, которая на вход принимает число и выдаёт его квадрат
+// (число умноженное на само себя).
+
+Console.Write ("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+//находим квадрат числа
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}.");
