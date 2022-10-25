2bc3410a783ecda70c27820b54d2f42c2013f6cb
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-10-25 21:36:26 +0300

    add Sem 009

---
+++ Sem_009/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+// 78 -> 8 
+// 12-> 2 
+// 85 -> 8
+
+int number  = new Random().Next(10, 100); 
+Console.WriteLine($"Исходное число: {number}");
+
+int digit1 = number % 10;
+int digit2 = number / 10;
+
+if (digit1 < digit2) 
+{
+    Console.WriteLine($"{digit2} - большая цифра");
+}
+else Console.WriteLine($"{digit1} - большая цифра");
