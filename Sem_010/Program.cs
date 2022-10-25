f81b70ad969019e7a4df073ee79b6eecbf9369c2
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-10-25 19:56:49 +0300

    add Sem 010

---
+++ Sem_010/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write ("Введите натуральное трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine (number > 99 & number < 1000);
+//Console.WriteLine (number > -1000 & number < -99);
+
+if ((number > 99 & number < 1000) | (number > -1000 & number < -99))
+{
+    number = number / 10;
+    Console.WriteLine (number % 10);
+}
+else
+{
+    Console.WriteLine ("Вы сделали ошибку при вводе");
+}
