9b5e5daf2917280cc8653ecd17d8998fcd2cf47c
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-10-27 12:16:35 +0300

    add Sem 023

---
+++ Sem_023/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход 
+// число (N) и выдает таблицу квадратов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write ("Введите натуральное число: ");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+if (numberN < 1) 
+{
+    Console.WriteLine ("Вы ввели неподходящее число");
+}
+else
+{
+    int index = 1;
+    while (index <= numberN)
+    {
+        Console.Write ($"{Math.Pow((index),3)} ");
+        index = index + 1;
+    }
+}
+Console.WriteLine ();
