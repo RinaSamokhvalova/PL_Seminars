2c80498b1d4f1ae521bd2f8f0b0c276700e313d3
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-10 16:04:31 +0300

    add Sem 024

---
+++ Sem_024/Program.cs
@@ -0,0 +1,30 @@
+﻿// Семинар 4.
+// Задача 24. Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// Решение без функций
+// Console.Write ("Введите натуральное число: ");
+// int numberN = int.Parse(Console.ReadLine() ?? "0");
+// int Sum = 0;
+// for (int i = 1; i <= numberN; i++)
+// {
+//     Sum = Sum + i;
+// }
+// Console.WriteLine(Sum);
+
+int GetSumNumbers (int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+       sum = sum + i;
+    }
+    return sum;
+}
+
+Console.Write ("Введите натуральное число: ");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine(GetSumNumbers(numberA));
