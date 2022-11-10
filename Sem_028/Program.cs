295f3ec9ce8f57b3974d92ba501d8c5d1f7ad362
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-10 17:04:37 +0300

    add Sem 028

---
+++ Sem_028/Program.cs
@@ -0,0 +1,20 @@
+﻿// Семинар 4.
+// Задача 28. Напишите программу, которая принимает на вход число N 
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ProductDig (int number)
+{
+    int prod = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        prod = prod * i; 
+    }
+    return prod;
+}
+
+Console.Write ("Введите натуральное число: ");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine ($"Исходное число: {numberN}");
+Console.WriteLine ($"Произведение чисел от 1 до {numberN}: {ProductDig (numberN)}");
