6f609d26e3e2b62938bfa4b31a5321b9b0ec5a99
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-16 06:14:20 +0300

    add Sem 032 - s5

---
+++ Sem_032/Program.cs
@@ -0,0 +1,43 @@
+﻿// Семинар 5.
+// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+//заполнить массив
+void FillArray(int[] array, int min, int max)
+{
+    int length = array.Length;
+    for (int index = 0; index < length; index++)
+    {
+        array[index] = new Random().Next(min, max + 1);
+    }
+}
+
+// Распечатать массив
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int index = 0; index < length; index++)
+    {
+        Console.Write ($"{array[index]} ");
+    }
+}
+
+Console.Write ("Введите размерность массива: ");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+
+int[] arrayN = new int[numberN];
+FillArray(arrayN, -10, 10);
+Console.Write ("Исходный массив: ");
+PrintArray(arrayN);
+Console.WriteLine ();
+
+int length = arrayN.Length;
+for (int index = 0; index < length; index++)
+{
+    arrayN[index] = arrayN[index] * (-1);
+}
+
+Console.Write ("Полученный массив: ");
+PrintArray(arrayN);
+Console.WriteLine ();
