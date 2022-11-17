4b430cf67e601c100239df3bae294aee13f313f8
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-17 18:46:17 +0300

    add Sem 038 - DZs5

---
+++ Sem_038/Program.cs
@@ -0,0 +1,39 @@
+﻿// Семинар 5. Домашнее задание.
+// Задача 38. Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+// Метод задает и распечатывает одномерный массив
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+    Console.WriteLine("Сформированный массив:");
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    Console.WriteLine ();
+    return res;
+}
+
+double Difference(double[] arr)
+{
+    double res = 0;
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+    }
+    res = max - min;
+    return res;
+}
+
+Console.Write ("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+double[] array = GetArray(size, -1000, 1000);
+double diff = Difference(array);
+
+Console.WriteLine($"Разницу между максимальным и минимальным элементами массива: {diff}");
