402907b6505cdd7affbff4418d2a95277c188ed8
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-24 01:08:39 +0300

    add Sem_056 - Seminar8_DZ

---
+++ Sem_056/Program.cs
@@ -0,0 +1,69 @@
+﻿// Семинар 8. Домашнее задание.
+// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine("Зададим матрицу размера M * N");
+Console.Write("Введите число строк M: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число столбцов N: ");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] matrix = GetMatrix(rows, columns, 0, 9);
+Console.WriteLine("Сформированный массив:");
+PrintMatrix(matrix);
+
+int index = MinSumElementRows(matrix);
+Console.WriteLine($"Наименьшеую суммоу элементов имеет строка {index}");
+
+// Методы
+
+int[,] GetMatrix(int sizeM, int sizeN, int minValue, int maxValue)
+{
+    int[,] res = new int[sizeM, sizeN];
+    for (int i = 0; i < sizeM; i++)
+    {
+        for (int j = 0; j < sizeN; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int MinSumElementRows(int[,] matr)
+{
+    int sum = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        sum = sum + matr[0, j];
+    }
+    int res = sum;
+    int resI = 0;
+    for (int i = 1; i < matr.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            sum = sum + matr[i, j];
+        }
+        if (res > sum) 
+        {
+            res = sum;
+            resI = i;
+        }
+    }
+    return resI;
+}
