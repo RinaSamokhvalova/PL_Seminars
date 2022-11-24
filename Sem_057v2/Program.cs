ff936972eb3b5f7d7b47aec4f2f7b30efa2eff75
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-24 11:14:54 +0300

    change Sem_057v2 - Seminar8

--- Sem_057v2/Program.cs
+++ Sem_057v2/Program.cs
@@ -28,51 +28,51 @@ void PrintMatrix(int[,] matr)
     Console.WriteLine();
 }
 
-void PrintMatrix2(int[,] matr) // распечатываем не весь массив, а только указанноу количество столбцов
-{
-    for (int i = 0; i < matr.GetLength(1); i++)
-    {
-        Console.WriteLine($"{matr[0, i]} встречается {matr[1, i]} ");
-    }
-}
+// void PrintMatrix2(int[,] matr) // распечатываем не весь массив, а только указанноу количество столбцов
+// {
+//     for (int i = 0; i < matr.GetLength(1); i++)
+//     {
+//         Console.WriteLine($"{matr[0, i]} встречается {matr[1, i]} ");
+//     }
+// }
 
-int[,] GetFrequencyArray(int[,] matr)
-{
-    int[,] tempfrequencyArray = new int[2, matr.GetLength(0) * matr.GetLength(1)];
-    int caunt = 0;
-    tempfrequencyArray[0, 0] = matr[0, 0];
-    bool flag = false;
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            flag = false;
-            for (int k = 0; k < caunt + 1; k++)
-            {
+// int[,] GetFrequencyArray(int[,] matr)
+// {
+//     int[,] tempfrequencyArray = new int[2, matr.GetLength(0) * matr.GetLength(1)];
+//     int caunt = 0;
+//     tempfrequencyArray[0, 0] = matr[0, 0];
+//     bool flag = false;
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             flag = false;
+//             for (int k = 0; k < caunt + 1; k++)
+//             {
 
-                if (matr[i, j] == tempfrequencyArray[0, k])
-                {
-                    tempfrequencyArray[1, k]++;
-                    flag = true;
-                }
-            }
-            if (flag == false)
-            {
-                caunt++;
-                tempfrequencyArray[0, caunt] = matr[i, j];
-                tempfrequencyArray[1, caunt] = 1;
+//                 if (matr[i, j] == tempfrequencyArray[0, k])
+//                 {
+//                     tempfrequencyArray[1, k]++;
+//                     flag = true;
+//                 }
+//             }
+//             if (flag == false)
+//             {
+//                 caunt++;
+//                 tempfrequencyArray[0, caunt] = matr[i, j];
+//                 tempfrequencyArray[1, caunt] = 1;
 
-            }
-        }
-    }
-    int[,] newFrequencyArray = new int[2, caunt + 1];
-    for (int i = 0; i < caunt + 1; i++)
-    {
-        newFrequencyArray[0, i] = tempfrequencyArray[0, i];
-        newFrequencyArray[1, i] = tempfrequencyArray[1, i];
-    }
-    return newFrequencyArray;
-}
+//             }
+//         }
+//     }
+//     int[,] newFrequencyArray = new int[2, caunt + 1];
+//     for (int i = 0; i < caunt + 1; i++)
+//     {
+//         newFrequencyArray[0, i] = tempfrequencyArray[0, i];
+//         newFrequencyArray[1, i] = tempfrequencyArray[1, i];
+//     }
+//     return newFrequencyArray;
+// }
 
 int[] GetRowArray(int[,] inArray)
 {
