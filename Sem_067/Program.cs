bc18fb7293e597238157c326e2544d4db70ddbd4
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-27 01:25:20 +0300

    seminar_9 add Sem_067

---
+++ Sem_067/Program.cs
@@ -0,0 +1,21 @@
+﻿// Семинар 9. 
+// Задача 67. Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12 
+// 45 -> 9
+
+Console.Write ("Введите натуральное число: ");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+if (numberN < 1)
+{
+    Console.WriteLine("Введено неподходящее число");
+}
+else
+{
+    Console.WriteLine ($"Сумма цифр в числе равна: {SusDigits(numberN)}");
+}
+
+int SusDigits(int number)
+{
+    if (number < 10) return number;
+    else return SusDigits(number / 10) + number % 10;
+}
