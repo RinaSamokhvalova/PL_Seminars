61903f68d144fda66f2c03f5d13d9b6124e06281
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-27 01:25:31 +0300

    seminar_9 add Sem_069

---
+++ Sem_069/Program.cs
@@ -0,0 +1,19 @@
+﻿// Семинар 9. 
+// Задача 69. Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243
+// A = 2; B = 3 -> 8
+
+// возведение в степень
+int Exponentiation(int number, int exp)
+{
+    if (exp == 1) return number;
+    else return Exponentiation(number, exp - 1) * number;
+}
+
+Console.Write ("Введите натуральное число A: ");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+Console.Write ("Введите натуральное число B: ");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine ($"Чисело {numberA} в степени {numberB} равняется: {Exponentiation (numberA, numberB)}");
