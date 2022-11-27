ed2d35da5ee264caa61a3ab0643d977cbf34f55d
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-11-27 12:48:58 +0300

    seminar_9 add Sem_066 DZ

---
+++ Sem_066/Program.cs
@@ -0,0 +1,24 @@
+﻿// Семинар 9. Домашнее задание.
+// Задача 66. Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число M: ");
+int numberM = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите натуральное число N: ");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+
+if (numberM > numberN)
+{
+    int temp = numberM;
+    numberM = numberN;
+    numberN = temp;
+}
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {numberM} до {numberN} равна {SumNaturalNumbers(numberM, numberN)}");
+
+int SumNaturalNumbers(int m, int n)
+{
+    if (n == m) return m;
+    else return SumNaturalNumbers(m, n - 1) + n;
+}
